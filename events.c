8ecd0edb97ae95d7107c67e11b2d30c5ab77f47a
Jaime Montoya <webmaster@jaimemontoya.com>
2015-11-01 20:31:15 -0500

    Changes to be committed: 	modified:   events.c

--- events.c
+++ events.c
@@ -1,8 +1,13 @@
-#include "linux/kernel.h"
-#include "linux/events.h"
+#include <linux/kernel.h>
+#include <linux/events.h>
+#include <linux/spinlock.h>
+#include <linux/slab.h>
 
-struct event_struct *events;
+#define MAX_EVENTS 1000000
+
+static struct event_struct *events;
 int nextID;
+DEFINE_SPINLOCK(event_lock);
 
 int doeventinit()
 {
@@ -12,12 +17,32 @@ int doeventinit()
 }
 
 asmlinkage long sys_doeventopen(void)
-{
-	return 0;
+{	
+	spin_lock(&event_lock);
+	if(nextID == MAX_EVENTS)
+		return -1;
+	struct event_struct *tmp = (struct event_struct *)kmalloc(sizeof(struct event_struct), GFP_KERNEL);
+	INIT_LIST_HEAD(&tmp->evtlist);
+	tmp->eventID = nextID;
+	nextID++;
+	if(events == NULL)	
+	{		
+		events = tmp;
+	}
+	else 
+	{	
+		list_add(&(tmp->evtlist), &(events->evtlist));
+	}
+	spin_unlock(&event_lock);	
+	printk("%i\n", tmp->eventID);
+	return tmp->eventID;
 }
 
 asmlinkage long sys_doeventclose(int eventID)
 {
+	spin_lock(&event_lock);
+	
+	spin_unlock(&event_lock);
 	return 0;
 }
 
