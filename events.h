efe6aa1332d5072bb91401db23ed9db6ae869cca
jaimemontoya <webmaster@jaimemontoya.com>
2015-11-05 14:05:35 -0500

    Update events.h

--- events.h
+++ events.h
@@ -5,25 +5,27 @@
 #include <linux/list.h>
 #include <linux/wait.h>
 
+/* Structure implementing our event abstraction */
 struct event_struct 
 {
-	int eventID;
-	uid_t UID;
-	gid_t GID;
-	int UIDFlag;
-	int GIDFlag;
-	int sig_flag;
-	int counter;
-	struct list_head evtlist;
-	wait_queue_head_t waitq;
+	int eventID;			// Unique ID of an event
+	uid_t UID;			// Effective UID associated with an event
+	gid_t GID;			// Effective GID associated with an event
+	int UIDFlag;			// User Signal Enable Bit
+	int GIDFlag;			// Group Signal Enable Bit
+
+	int sig_flag;			// Signal flag 
+					// 0 - No signal, some processes are currently sleeping on this event
+					// 1 - Signal is set
+					
+	int counter;			// The number of processes currently sleeping on this event
+	struct list_head evtlist;	// Linux list structure
+	wait_queue_head_t waitq;	// Waiting queue for this event
 };
 
 typedef struct event_struct event_t;
 
+/* Init function for events */
 void __init doevent_init(void);
 
 #endif
-
-
-
-
