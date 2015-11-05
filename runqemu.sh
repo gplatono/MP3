32a329e7dd56f7cb366553a8ca2eb5af57ab1546
jaimemontoya <webmaster@jaimemontoya.com>
2015-11-05 14:07:08 -0500

    Update runqemu.sh

--- runqemu.sh
+++ runqemu.sh
@@ -9,5 +9,5 @@ then
 	./install/bin/qemu-system-x86_64 -m 64M -L ./install/qemu/share/qemu/ -nographic -hda ./openwrt-15.05-x86-generic-combined-ext4.img -fda fat:floppy:`pwd`/floppy
 else
 	echo "Running QEMU with your kernel at $1"
-	./install/bin/qemu-system-x86_64 -m 64M -L ./install/qemu/share/qemu/ -nographic -kernel $1 -hda ./openwrt-15.05-x86-generic-combined-ext4.img -append "root=PARTUUID=076b2e55-02 rootfstype=ext4 rootwait console=tty0 console=ttyS0,38400n8 noinitrd" -hdb floppy/eventtest -fda fat:floppy:`pwd`/floppy
+	./install/bin/qemu-system-x86_64 -m 64M -L ./install/qemu/share/qemu/ -nographic -kernel $1 -hda ./openwrt-15.05-x86-generic-combined-ext4.img -append "root=PARTUUID=076b2e55-02 rootfstype=ext4 rootwait console=tty0 console=ttyS0,38400n8 noinitrd" -hdb floppy/test -fda fat:floppy:`pwd`/floppy
 fi
